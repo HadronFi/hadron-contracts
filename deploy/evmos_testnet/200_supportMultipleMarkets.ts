76c45521f5642f46352fb15ba84940e44562a6a3
a <a@proton.me>
2022-10-30 00:27:52 -0700

    Update collateral factor

--- deploy/evmos_testnet/200_supportMultipleMarkets.ts
+++ deploy/evmos_testnet/200_supportMultipleMarkets.ts
@@ -10,31 +10,54 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer, admin } = await getNamedAccounts()
 
   const markets = [
-    (await get("crWEVMOS")).address,
-    (await get("crUSDC")).address,
-    (await get("crUSDT")).address
-  ];
-  
-  const cfs = [
-    parseEther('0.75'),
-    parseEther('0.75'),
-    parseEther('0.75'),
-  ];
-  
-  const rfs = [
-    parseEther('0.20'),
-    parseEther('0.20'),
-    parseEther('0.20')
-  ];
-  
+    (await get('crWEVMOS')).address,
+    (await get('crUSDC')).address,
+    (await get('crUSDT')).address,
+  ]
 
-  for (let i=0; i < markets.length; i++) {
-    await execute('CTokenAdmin', { from: deployer }, '_setReserveFactor', markets[i], rfs[i]);
-    await execute('Comptroller', { from: deployer }, '_supportMarket', markets[i], 1);
-    await execute('Comptroller', { from: deployer }, '_setCollateralFactor', markets[i], cfs[i]);
+  const cfs = [parseEther('0.75'), parseEther('0.75'), parseEther('0.75')]
+
+  const rfs = [parseEther('0.20'), parseEther('0.20'), parseEther('0.20')]
+
+  const prices = [parseEther('1.5'), parseEther('1'), parseEther('1')]
+
+  for (let i = 0; i < markets.length; i++) {
+    await execute(
+      'SimplePriceOracle',
+      { from: deployer, log: true },
+      'setUnderlyingPrice',
+      markets[i],
+      prices[i],
+    )
+    await execute(
+      'CTokenAdmin',
+      { from: deployer, log: true },
+      '_setReserveFactor',
+      markets[i],
+      rfs[i],
+    )
+    await execute(
+      'Comptroller',
+      { from: deployer, log: true },
+      '_supportMarket',
+      markets[i],
+      1,
+    )
+    await execute(
+      'Comptroller',
+      { from: deployer, log: true },
+      '_setCollateralFactor',
+      markets[i],
+      cfs[i],
+    )
   }
 
-  await execute('FlashloanLender', { from: deployer }, 'updateUnderlyingMapping', markets);
+  await execute(
+    'FlashloanLender',
+    { from: deployer },
+    'updateUnderlyingMapping',
+    markets,
+  )
 }
 export default func
 func.tags = ['Market']
