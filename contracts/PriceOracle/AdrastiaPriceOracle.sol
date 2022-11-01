f967db1d9b25a48a1e0b4b8feec7ba95aaf6aafe
a <a@proton.me>
2022-11-01 09:46:44 -0700

    Finish Adrastia price oracle

--- contracts/PriceOracle/AdrastiaPriceOracle.sol
+++ contracts/PriceOracle/AdrastiaPriceOracle.sol
@@ -1,31 +1,8 @@
 pragma solidity ^0.6.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @title IPriceOracle
- * @notice An interface that defines a price and liquidity oracle.
- */
 interface IPriceOracle {
     function consultPrice(address token, uint256 maxAge) external view returns (uint112 price);
-}
-
-interface IOracle{
-    function consult(address token)
-        external
-        view
-        returns (
-            uint112 price,
-            uint112 tokenLiquidity,
-            uint112 quoteTokenLiquidity
-        );
-
-    function consult(address token, uint256 maxAge)
-        external
-        view
-        returns (
-            uint112 price,
-            uint112 tokenLiquidity,
-            uint112 quoteTokenLiquidity
-        );
 }
 
 interface ICErc20{
@@ -42,55 +19,53 @@ contract AdrastiaPriceOracle {
     );
     event ExpiredPrice(address asset, address oracle);
     event Price(uint112 price);
-    address oracle = 0x51d3d22965Bb2CB2749f896B82756eBaD7812b6d;
 
-    function setOracleAddress(address addr) public {
-        oracle = addr;
+    address aggregatedOracle = 0x51d3d22965Bb2CB2749f896B82756eBaD7812b6d;
+    address usdPeggedAggregatedOracle = 0xd850F64Eda6a62d625209711510f43cD49Ef8798;
+    address constant WEVMOS = 0xD4949664cD82660AaE99bEdc034a0deA8A0bd517;
+
+    function setOracleAddress(address aggregated, address usdPeggedAggregated) public onlyOwner {
+        require(aggregated != address(0), "Aggregated addr not set");
+        require(usdPeggedAggregated != address(0), "usdPeggedAggregated addr not set");
+        aggregatedOracle = aggregated;
+        usdPeggedAggregatedOracle = usdPeggedAggregated;
     }
 
-    function getPrice(address token, uint256 maxAge) public returns (uint256){
-        uint112 adrastiaPrice = IPriceOracle(oracle).consultPrice(token, 0);
-        return uint256(adrastiaPrice);
-    }
-
-    function getOraclePrice(address token, uint256 maxAge) public returns (uint256){
-        (
-            uint112 price,
-            uint112 tokenLiquidity,
-            uint112 quoteTokenLiquidity
-        ) = IOracle(oracle).consult(token, maxAge);
-        return uint256(price);
-    }
-
+    /**
+     * @notice Get the underlying price of a cToken asset
+     * @param cToken The cToken to get the underlying price of
+     * @return The underlying asset price mantissa (scaled by 1e18).
+     *  Zero means the price is unavailable.
+     */
     function getUnderlyingPrice(address cToken) public view returns (uint256) {
         address underlyingAsset = ICErc20(address(cToken)).underlying();
-        uint256 price;
+        uint256 evmosPrice;
+        uint256 assetPrice;
         // Gets the price of `token` with the requirement that the price is 2 hours old or less
-        try IPriceOracle(oracle).consultPrice(underlyingAsset, 2 hours) returns (uint112 adrastiaPrice) {
-            price = uint256(adrastiaPrice);
+        try IPriceOracle(usdPeggedAggregatedOracle).consultPrice(underlyingAsset, 2 hours) returns (uint112 adrastiaPrice) {
+            evmosPrice = uint256(adrastiaPrice);
         } catch (bytes memory) {
-            price = backupPrices[underlyingAsset];
+            evmosPrice = backupPrices[underlyingAsset];
         }
-        return uint256(price);
+        // USD Pegged Aggregated Oracle is denominated with 6 decimals
+        if (underlyingAsset == WEVMOS){
+            return evmosPrice * 1e12;
+        }
+        try IPriceOracle(aggregatedOracle).consultPrice(underlyingAsset, 2 hours) returns (uint112 adrastiaPrice) {
+            assetPrice = uint256(adrastiaPrice);
+        } catch (bytes memory) {
+            assetPrice = backupPrices[underlyingAsset];
+        }
+        return uint256(evmosPrice * assetPrice / 1e6);
     }
 
-    function setUnderlyingPrice(address cToken, uint256 underlyingPriceMantissa) public {
-        address asset = address(ICErc20(address(cToken)).underlying());
-        emit PricePosted(asset, backupPrices[asset], underlyingPriceMantissa, underlyingPriceMantissa);
-        backupPrices[asset] = underlyingPriceMantissa;
-    }
-
-    function setDirectPrice(address asset, uint256 price) public {
+    /**
+     * @notice set the price of an asset
+     * @param asset The address of the asset
+     * @param price The price to set the asset to
+     */
+    function setDirectPrice(address asset, uint256 price) public onlyOwner {
         emit PricePosted(asset, backupPrices[asset], price, price);
         backupPrices[asset] = price;
-    }
-
-    // v1 price oracle interface for use as backing of proxy
-    function assetPrices(address asset) external view returns (uint256) {
-        return backupPrices[asset];
-    }
-
-    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
 }
